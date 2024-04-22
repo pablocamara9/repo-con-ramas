828a2b4a9821b195a4e280fb4070abea60f45ff0
Pablo Cámara García <camara.gapab23@triana.salesianos.edu>
2024-04-22 13:14:37 +0200

    First commit

---
+++ ProyectoRepositorioConRamas/src/main/java/com/salesianostriana/dam/proyectorepositorioconramas/ProyectoRepositorioConRamasApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.proyectorepositorioconramas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoRepositorioConRamasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoRepositorioConRamasApplication.class, args);
+	}
+
+}
